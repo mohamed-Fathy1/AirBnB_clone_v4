b22e53662c4c2a8f1619a4200370481d550c474c
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-11-16 22:18:27 +0200

    Add fetching reviwes

---
+++ web_dynamic/static/scripts/101-hbnb.js
@@ -0,0 +1,212 @@
+
+let selectedAminites_list = []
+let selectedAminites_dict = {}
+let selectedStates_list = []
+let selectedStates_dict = {}
+let selectedCities_list = []
+let selectedCities_dict = {}
+let offset = 1
+
+function append_text_to_list() {
+    states = selectedStates_list.map(ele => `<b> ${ele} </b>`)
+    states = states.join(', ')
+    cities = selectedCities_list.join(', ')
+    $('.locations h4').html(cities && states ? states + ', ' + cities : states ? states : cities)
+}
+
+
+function checkAminites(){
+    $('.amenities .popover li input').on('click',
+        function () {
+            const data_id = $(this).attr("data-id")
+            const data_name = $(this).attr("data-name")
+            if ($(this).is(":checked")) {
+                selectedAminites_list.push(data_name)
+                selectedAminites_dict[data_name] = data_id
+            } else {
+                selectedAminites_list = selectedAminites_list.filter((e, i) => e !== data_name);
+                delete selectedAminites_dict[data_name]
+            }
+            $('.amenities h4').html(selectedAminites_list.join(', '))
+        })
+}
+
+
+function checkStates(){
+    $('.locations .popover > ul > li > input').on('click',
+        function () {
+            const data_id = $(this).attr("data-id")
+            const data_name = $(this).attr("data-name")
+            if ($(this).is(":checked")) {
+                selectedStates_list.push(data_name)
+                selectedStates_dict[data_name] = data_id
+            } else {
+                selectedStates_list = selectedStates_list.filter((e, i) => e !== data_name);
+                delete selectedStates_dict[data_name]
+            }
+            append_text_to_list()
+        })
+}
+
+
+
+function checkCities(){
+    $('.locations .popover > ul ul li input').on('click',
+        function () {
+            const data_id = $(this).attr("data-id")
+            const data_name = $(this).attr("data-name")
+            if ($(this).is(":checked")) {
+                selectedCities_list.push(data_name)
+                selectedCities_dict[data_name] = data_id
+            } else {
+                selectedCities_list = selectedCities_list.filter((e, i) => e !== data_name);
+                delete selectedCities_dict[data_name]
+            }
+            append_text_to_list()
+        })
+}
+
+
+function check_api_status(){
+    $(() => {
+        $.get('http://0.0.0.0:5001/api/v1/status/', (data) => {
+            if (data.status === 'OK') {
+                $('div#api_status').addClass('available');
+            } else {
+                $('div#api_status').removeClass('available');
+            }
+        })
+    })
+}
+
+
+function loadMore() {
+    $('section.places .load-more button').click(() => {
+        offset++
+        $('section.places .load-more button').remove()
+        $('section.places').append(`
+                         <div class="loading">
+                            <div class="loading-spin"></div>
+                         </div>
+                    `)
+
+        let aminity_ids = Object.values(selectedAminites_dict)
+        let state_ids = Object.values(selectedStates_dict)
+        let city_ids = Object.values(selectedCities_dict)
+
+        let search_data = {
+            "states": state_ids,
+            "cities": city_ids,
+            "amenities": aminity_ids,
+            "offset": offset,
+        }
+        fillter_search(search_data)
+    })
+
+}
+
+
+function getPlaces() {
+    $('section.filters button').click(() => {
+        offset = 1
+        $('section.places').empty();
+        $('section.places').html(`
+                         <div class="loading">
+                            <div class="loading-spin"></div>
+                         </div>
+                    `)
+        let aminity_ids = Object.values(selectedAminites_dict)
+        let state_ids = Object.values(selectedStates_dict)
+        let city_ids = Object.values(selectedCities_dict)
+
+        let search_data = {
+            "states": state_ids,
+            "cities": city_ids,
+            "amenities": aminity_ids,
+            "offset": offset,
+        }
+        fillter_search(search_data)
+    })
+}
+
+
+function fillter_search(search_data) {
+    $.ajax({
+        type: "POST",
+        url: 'http://0.0.0.0:5001/api/v1/places_search?limit=10',
+        data: JSON.stringify(search_data),
+        contentType: 'application/json',
+        success: (data) => {
+            if (offset === 1)
+                $('section.places').empty();
+            else
+                $('section .loading').remove()
+
+            if (data.length === 0)
+                return
+
+            data.forEach((place) => {
+                let newPlace = `<article data-id=${place.id}>
+                    <div class="title_box">
+                        <h2>${place.name}</h2>
+                        <div class="price_by_night">$${place.price_by_night}</div>
+                    </div>
+                    <div class="information">
+                    <div class="max_guest">${place.max_guest} Guest${(place.max_guest != 1) ? 's' : ''}</div>
+                    <div class="number_rooms">${place.number_rooms} Bedroom${(place.number_rooms != 1) ? "s" : ""}</div>
+                    <div class="number_bathrooms">${place.number_bathrooms} Bathroom${(place.number_bathrooms != 1) ? "s" : ''}</div>
+                    </div>
+                    <div class="user">
+                    </div>
+                    <div class="description">
+                    ${place.description}
+                    </div>
+                    <div class="reviews-container">
+                    <div class="reviews">
+                        <h2 style="width: 20px;"></h2>
+                        <h2>Reviews</h2>
+                        <span class="toggle-reviews">Show <i class="toggle-arrow"></i></span>
+                    </div>
+                    </div>
+                    </article>`
+                $('section.places').append(newPlace);
+            })
+            $('section.places').append(`<div class="load-more"> <button>LOAD MORE</button> </div>`)
+            getReviews()
+            loadMore()
+        },
+        error: function (error) {
+            console.error('Error:', error);
+        }
+    })
+}
+
+
+function getReviews() {
+    $('div.reviews span.toggle-reviews').click(function () {
+        placeID = $(this).parent().parent().parent().attr('data-id')
+        url = `http://0.0.0.0:5001/api/v1/places/${placeID}/reviews`
+        $.get(url, (data) =>{
+            console.log($(this).parent())
+            data.forEach(review => {
+                $(this).parent().parent().append(`
+                <ul>
+                    <li>
+                        <p>${review.text}</p>
+                    </li>
+                </ul>
+                `)
+            })
+        })
+    })
+}
+
+$( document ).ready(()=>{
+    $('input[type=checkbox]:checked').prop('checked', false)
+    check_api_status()
+    checkAminites()
+    checkStates()
+    checkCities()
+    getPlaces()
+
+});
